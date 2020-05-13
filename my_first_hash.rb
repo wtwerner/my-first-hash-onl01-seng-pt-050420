ef0c2407c708cc091b6b3b8a1b6ec600ef6195ed
Tommy Werner <wtwerner@gmail.com>
2020-05-13 23:34:17 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -32,12 +32,12 @@ def adding
                  }
   # add 2 muskets to the shipping_manifest hash below
 
-
+    shipping_manifest["muskets"] = 2
 
   # add 4 gun powder to the shipping_manifest hash below
 
-
-
+    shipping_manifest["gun powder"] = 3
+  
   # return the shipping_manifest hash below
 
 end
